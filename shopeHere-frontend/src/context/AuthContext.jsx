10829bc671d3ce37fda5268be183ba45484e1173
Rohit singh <141452252+RohitsRajput@users.noreply.github.com>
2026-01-12 22:32:44 +0530

    Update AuthContext.jsx

--- shopeHere-frontend/src/context/AuthContext.jsx
+++ shopeHere-frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 export const authDataContext = createContext();
 
 function AuthContext({ children }) {
-  const serverUrl = "https://shop-here-euz7.onrender.com";
+  const serverUrl = "http://localhost:5000";
   return (
     <div>
       <authDataContext.Provider value={{ serverUrl }}>
