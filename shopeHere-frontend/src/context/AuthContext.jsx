0d748f3560493963148f8cb0125275560635e21b
Rohit singh <141452252+RohitsRajput@users.noreply.github.com>
2026-01-12 21:30:54 +0530

    Update server URL to production environment

--- shopeHere-frontend/src/context/AuthContext.jsx
+++ shopeHere-frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 export const authDataContext = createContext();
 
 function AuthContext({ children }) {
-  const serverUrl = "http://localhost:5000";
+  const serverUrl = "https://shop-here-euz7.onrender.com";
   return (
     <div>
       <authDataContext.Provider value={{ serverUrl }}>
