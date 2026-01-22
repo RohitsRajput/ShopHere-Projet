c603c4e7fcc4deece234abced1ca49f333ebbfa3
RohitsRajput <as.rohitsingh.03@gmail.com>
2026-01-22 21:20:29 +0530

    Fix frontend import

--- shopeHere-frontend/src/main.jsx
+++ shopeHere-frontend/src/main.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import AuthContext from "./context/authContext.jsx";
-import UserContext from "./context/userContext.jsx";
-import ShopContext from "./context/ShopContext.jsx";
+
+import AuthContext from "./context/authContext";
+import UserContext from "./context/userContext";
+import ShopContext from "./context/ShopContext";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
