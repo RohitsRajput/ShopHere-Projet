0839764f94284d57f8d4bb7547978b3c08ec1163
RohitsRajput <as.rohitsingh.03@gmail.com>
2026-01-31 22:12:38 +0530

    chnage frontend main

--- shopeHere-frontend/src/main.jsx
+++ shopeHere-frontend/src/main.jsx
@@ -2,16 +2,15 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-<<<<<<< HEAD
 
-import AuthContext from "./context/authContext";
+import AuthContext from "./context/AuthContext";
 import UserContext from "./context/userContext";
 import ShopContext from "./context/ShopContext";
-=======
-import AuthContext from "./context/authContext.jsx";
+
+import AuthContext from "./context/AuthContext.jsx";
 import UserContext from "./context/userContext.jsx";
 import ShopContext from "./context/ShopContext.jsx";
->>>>>>> mains
+
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
